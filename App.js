2b4d36052c313f36422c0a31874606e5d08c06cd
Janier Rodríguez López <janierbits@yahoo.com>
2023-04-04 00:14:32 -0400

    fixed App indentation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { ThemeProvider } from "react-native-rapi-ui";
+
 export default function App() {
   return (
     <ThemeProvider>
