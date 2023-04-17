d7253c9f9f5212c41e87bf77c73e1af55e0cf716
Janier Rodríguez López <janierbits@yahoo.com>
2023-04-17 19:40:21 -0400

    fixed send text message

--- src/screens/RecordScreen.js
+++ src/screens/RecordScreen.js
@@ -13,8 +13,6 @@ import {
     useTheme,
 } from "react-native-rapi-ui";
 //import {Video} from "expo-av"
-import { AntDesign ,Entypo,FontAwesome} from '@expo/vector-icons';
-//import { contains } from "@firebase/util";
 
 const Record =  ({navigation})=>{
 
@@ -22,7 +20,7 @@ const Record =  ({navigation})=>{
     //const videoStream = new MediaStream();
     const ws= React.useRef()
     const [status,setStatus] = React.useState({})
-    const socket = io.connect("http://192.168.1.100:3001");
+    const socket = io.connect("http://192.168.1.102:3001");
     const { isDarkmode } = useTheme();
 
     
@@ -34,17 +32,17 @@ const Record =  ({navigation})=>{
       socket.emit("pausa");
       console.log("PAUSA");
     };
-   const continuaMessage =()=>{
-    socket.emit("continua");
-    console.log("CONTINUA");
-  };
+    const continuaMessage =()=>{
+      socket.emit("continua");
+      console.log("CONTINUA");
+    };
     const stopMessage =()=>{
         socket.emit("stop");
         console.log("Stop");
     };
     const downloadMessage =()=>{
-      socket.emit("stream");
-      console.log("Bajalo");
+      socket.emit("upload");
+      console.log("Subelo");
     };
     return(
     
@@ -74,21 +72,21 @@ const Record =  ({navigation})=>{
                 style={{ marginTop: 10 }}
                 text="Comenzar Grabación" onPress={startMessage}
                 leftContent={
-                  <Entypo name="controller-play" size={24} color="black" />
+                  <Ionicons name="play" size={24} color="black" />
                 }
               />
               <Button
                 style={{ marginTop: 10 }}
                 text="Pausar Grabación" onPress={pausaMessage}
                 leftContent={
-                  <AntDesign name="pausecircle" size={24} color="black" />
+                  <Ionicons name="pause" size={24} color="black" />
                 }
               />
               <Button
                 style={{ marginTop: 10 }}
                 text="Continuar Grabación" onPress={continuaMessage}
                 leftContent={
-                  <AntDesign name="stepforward" size={24} color="black" />
+                  <Ionicons name="play-skip-forward" size={24} color="black" />
                 }
               />
               <Button
@@ -97,14 +95,14 @@ const Record =  ({navigation})=>{
                   marginTop: 10,
                 }}
                 leftContent={
-                  <Entypo name="controller-stop" size={24} color="black" />
+                  <Ionicons name="stop" size={24} color="black" />
                 }
               />
               <Button
                 style={{ marginTop: 10 }}
                 text="Descargar Archivo" onPress={downloadMessage}
                 leftContent={
-                  <Entypo name="arrow-down" size={24} color="black" />
+                  <Ionicons name="cloud-upload" size={24} color="black" />
                 }
               />
 
@@ -117,7 +115,7 @@ const Record =  ({navigation})=>{
                 marginTop: 10,
               }}
               leftContent={
-                <FontAwesome name="commenting" size={24} color="black" />
+                <Ionicons name="chatbox-ellipses" size={24} color="black" />
               }
             />
             </SectionContent>
